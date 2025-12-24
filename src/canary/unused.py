6be6c99c3915b7461a69206c97fe1a890826a6af
Justin <justin.guida@snhu.edu>
2025-12-23 22:29:40 -0500

    chore: add real canary failures

---
+++ src/canary/unused.py
@@ -0,0 +1,59 @@
+def unused_func_1():
+    return 1
+
+def unused_func_2():
+    return 2
+
+def unused_func_3():
+    return 3
+
+def unused_func_4():
+    return 4
+
+def unused_func_5():
+    return 5
+
+def unused_func_6():
+    return 6
+
+def unused_func_7():
+    return 7
+
+def unused_func_8():
+    return 8
+
+def unused_func_9():
+    return 9
+
+def unused_func_10():
+    return 10
+
+def unused_func_11():
+    return 11
+
+def unused_func_12():
+    return 12
+
+def unused_func_13():
+    return 13
+
+def unused_func_14():
+    return 14
+
+def unused_func_15():
+    return 15
+
+def unused_func_16():
+    return 16
+
+def unused_func_17():
+    return 17
+
+def unused_func_18():
+    return 18
+
+def unused_func_19():
+    return 19
+
+def unused_func_20():
+    return 20
